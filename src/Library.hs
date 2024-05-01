f65ec0a87f494c16ca3cd2eaa982de0912863b1e
Tomas Rios <torios@frba.utn.edu.ar>
2024-05-01 15:35:50 -0300

    info inicial

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,18 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Cancion = String
+
+data Artista = UnArtista {
+    nombre :: String,
+    canciones :: [Cancion]
+} deriving Show
+
+fitito :: Artista
+fitito = UnArtista "Fitito Paez" ["11 y 6", "El amor despues del amor", "Mariposa Tecknicolor"]
+
+calamardo :: Artista
+calamardo = UnArtista "Andres Calamardo" ["Flaca", "Sin Documentos", "Tuyo siempre"]
+
+paty :: Artista
+paty = UnArtista "Taylor Paty" ["Shake It Off", "Lover"]
