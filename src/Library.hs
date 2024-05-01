6d69e6a1829d9eaad2c0e423395f48faadf650ed
tommyrios <torios@frba.utn.edu.ar>
2024-05-01 20:03:32 -0300

    version inicial, falta terminar punto4

--- src/Library.hs
+++ src/Library.hs
@@ -16,3 +16,36 @@ calamardo = UnArtista "Andres Calamardo" ["Flaca", "Sin Documentos", "Tuyo siemp
 
 paty :: Artista
 paty = UnArtista "Taylor Paty" ["Shake It Off", "Lover"]
+
+--punto 1
+
+calificacion :: Cancion -> Number
+calificacion = (+ 10) . length  . filter (`elem` ['a'..'z'])
+
+--punto 2
+
+cancionBuena :: Cancion -> Bool
+cancionBuena = (>20) . calificacion
+
+cancionesBuenas :: Artista -> [Cancion]
+cancionesBuenas artista = filter cancionBuena (canciones artista)
+
+esExitoso :: Artista -> Bool
+esExitoso artista = (> 50) . sum . map calificacion $ cancionesBuenas artista
+
+--punto 3
+
+grupo :: [Artista]
+grupo = [fitito, calamardo, paty]
+
+artistasExitosos :: [Artista] -> [Artista]
+artistasExitosos = filter esExitoso
+
+--punto 4
+
+todoJunto :: [Artista] -> [Artista]
+todoJunto artistas = filter (\artista -> sum (map (\cancion -> (>20) ((length . filter (`elem` ['a'..'z'])) cancion + 10)) (canciones artista)) > 50) artistas
+
+
+
+--
